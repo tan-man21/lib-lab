2b2331963dd37ef85b35f496bc36ba040bfa9e8a
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-06-04 12:11:35 -0400

    added navbar and hero image

---
+++ client/src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+
+function Hero() {
+    return (
+        <div className="heroContainer px-2 pt-3 mb-4 d-flex align-items-center">
+            <h1 className="heroText">Welcome to LibLab</h1>
+        </div>
+    )
+}
+
+export default Hero
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { CurrentUser } from '../contexts/CurrentUser';
+
+function NavBar() {
+
+    // const { currentUser } = useContext(CurrentUser)
+
+  return (
+        <Navbar key={'lg'} expand={'lg'} className="bg-body-tertiary" sticky='top'>
+          <Container fluid>
+            <Navbar.Brand href="/">LibLab</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3" variant='underline'>
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/action2">Explore Books</Nav.Link>
+                  <Nav.Link href="/action3">My Books</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>)}
+
+export default NavBar;
